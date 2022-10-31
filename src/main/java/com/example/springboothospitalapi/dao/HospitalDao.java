e46a865972487cdbbefbd9c5c6f72a1881d1b0bf
Jiyoung <ahn.jiyoung15@gmail.com>
2022-10-31 16:15:30 +0900

    hospital dao - add, deleteall

---
+++ src/main/java/com/example/springboothospitalapi/dao/HospitalDao.java
@@ -0,0 +1,41 @@
+package com.example.springboothospitalapi.dao;
+
+import com.example.springboothospitalapi.domain.Hospital;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HospitalDao {
+    private final JdbcTemplate jdbcTemplate;
+
+    public HospitalDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+    public int add(Hospital hospital){
+        return this.jdbcTemplate.update("INSERT INTO hospital(id,open_service_name,open_local_government_code," +
+                "management_number, license_date, business_status," +
+                "business_status_code, phone,full_address," +
+                "road_name_address,hospital_name,business_type_name," +
+                "healthcare_provider_count,patient_room_count,total_number_of_beds) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
+                hospital.getId(),
+                hospital.getOpenServiceName(),
+                hospital.getOpenLocalGovernmentCode(),
+                hospital.getManagementNumber(),
+                hospital.getLicenseDate(),
+                hospital.getBusinessStatus(),
+                hospital.getBusinessStatusCode(),
+                hospital.getPhone(),
+                hospital.getFullAddress(),
+                hospital.getRoadNameAddress(),
+                hospital.getHospitalName(),
+                hospital.getBusinessTypeName(),
+                hospital.getHealthcareProviderCount(),
+                hospital.getPatientRoomCount(),
+                hospital.getTotalNumberOfBeds()
+        );
+    }
+    public int deleteAll(){
+        return this.jdbcTemplate.update("DELETE FROM hospital");
+    }
+}
