72e0895f6b4585847153a1b393a85f25b01e1988
Jiyoung <ahn.jiyoung15@gmail.com>
2022-10-31 15:34:49 +0900

    autowired, 만건 이상 test

--- src/test/java/com/example/springboothospitalapi/parser/HospitalParserTest.java
+++ src/test/java/com/example/springboothospitalapi/parser/HospitalParserTest.java
@@ -1,7 +1,7 @@
 package com.example.springboothospitalapi.parser;
 
 import com.example.springboothospitalapi.domain.Hospital;
-록import org.apache.catalina.core.ApplicationContext;
+import org.apache.catalina.core.ApplicationContext;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
