b35f8a7338125fcff5a205e7ed2807b8685d7218
Jiyoung <ahn.jiyoung15@gmail.com>
2022-10-31 11:25:38 +0900

    -

---
+++ src/main/java/com/example/springboothospitalapi/SpringBootHospitalApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboothospitalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHospitalApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHospitalApiApplication.class, args);
+    }
+
+}
