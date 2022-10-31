cf99bf48de775d7cf71984d65032d0ad508dd807
Jiyoung <ahn.jiyoung15@gmail.com>
2022-10-31 14:48:23 +0900

    replace 수정

--- src/main/java/com/example/springboothospitalapi/parser/HospitalParser.java
+++ src/main/java/com/example/springboothospitalapi/parser/HospitalParser.java
@@ -9,7 +9,7 @@ public class HospitalParser implements Parser<Hospital> {
     @Override
     public Hospital parse(String str) {
         //str = "1,의원,01_01_02_P,3620000,PHMA119993620020041100004,19990612,,1,영업/정상,13,영업중,,,,,062-515-2875,,500881,광주광역시 북구 풍향동 565번지 4호 3층,"광주광역시 북구 동문대로 24, 3층 (풍향동)",61205,효치과의원,2.02111E+13,U,2021.11.17 2:40,치과의원,192630.7351,185314.6176,치과의원,1,0,0,52.29,401,치과,,,,0,0,,,0,"
-        String[] arr = str.replace(", ",". ").split(",");
+        String[] arr = str.replace(", ",".").split(",");
         Hospital hospital = new Hospital();
         hospital.setId(Integer.parseInt(arr[0]));
         hospital.setOpenServiceName(arr[1]);
@@ -25,7 +25,7 @@ public class HospitalParser implements Parser<Hospital> {
         hospital.setBusinessStatusCode(Integer.parseInt(arr[9]));
         hospital.setPhone(arr[15]);
         hospital.setFullAddress(arr[18]);
-        hospital.setRoadNameAddress(arr[19].replace(".",",").replace("\"",""));
+        hospital.setRoadNameAddress(arr[19].replace(".",", ").replace("\"",""));
         hospital.setHospitalName(arr[21]);
         hospital.setBusinessTypeName(arr[25]);
         hospital.setHealthcareProviderCount(Integer.parseInt(arr[29]));
